1e59862fc4ad861df765e56a530c00750c8fa5c3
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-22 21:03:49 +0300

    До 2.6 главы

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,69 @@
 <template>
-  <h1>Очистили стартовый проект</h1>
+  <v-app>
+  <v-navigation-drawer app v-model="drawer">
+  <v-list-item>
+  <v-list-item-content>
+  <v-list-item-title class="title">
+  КИПУ
+  </v-list-item-title>
+  <v-list-item-subtitle>
+  Учебный проект
+  </v-list-item-subtitle>
+  </v-list-item-content>
+  </v-list-item>
+  <v-divider></v-divider>
+  <v-list dense>
+  <v-list-item
+  v-for="link in links"
+  :key="link.title"
+  >
+  <template v-slot:prepend>
+  <v-icon :icon="link.icon"></v-icon>
   </template>
+  <v-list-item-title>{{ link.title }}</v-list-item-title>
+  </v-list-item>
+  </v-list>
+  </v-navigation-drawer>
+  <v-app-bar app dark color="primary">
+  <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+  <v-spacer></v-spacer>
+  <v-toolbar-items class="hidden-sm-and-down">
+  <v-btn
+  text
+  v-for="link in links"
+  :key="link.title"
+  >
+  <v-icon
+  start
+  :icon="link.icon"
+  ></v-icon>
+  {{ link.title }}
+  </v-btn>
+  </v-toolbar-items>
+  </v-app-bar>
+  <v-main>
+  <router-view></router-view>
+  </v-main>
+  </v-app>
+  </template>
+  
   
   
   <script>
   export default {
+  data() {
+  return {
+  drawer: false,
+  links: [
+  {title:"Login", icon:"mdi-lock", url:"/login"},
+  {title:"Registration",icon:"mdi-face",url:"/registration"},
+  {title:"Orders",icon:"mdi-bookmark-multiple-outline", url:"/orders"},
+  {title:"New ad", icon:"mdi-note-plus-outline", url:"/new"},
+  {title:"My ads", icon:"mdi-view-list-outline", url:"/list"}
+  ]
+  }
+  }
   }
   </script>
+  
   
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  <v-container>
+  <v-row justify="center">
+  <h1>Home</h1>
+  </v-row>
+  </v-container>
+  </template>
+  <script>
+  export default {
+  data () {
+  return {
   }
-}
-</script>
+  }
+  }
+  </script>
+  
