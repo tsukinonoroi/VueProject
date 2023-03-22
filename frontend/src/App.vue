3480815ea3bae8db4e0b8e29f1a0dacc071be276
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-03-22 21:18:52 +0300

    2.8 Глава

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,31 +13,35 @@
   </v-list-item>
   <v-divider></v-divider>
   <v-list dense>
-  <v-list-item
-  v-for="link in links"
-  :key="link.title"
-  >
-  <template v-slot:prepend>
-  <v-icon :icon="link.icon"></v-icon>
-  </template>
-  <v-list-item-title>{{ link.title }}</v-list-item-title>
-  </v-list-item>
+    <v-list-item
+    v-for="link in links"
+    :key="link.title"
+    :to="link.url"
+    >
+    <template v-slot:prepend>
+    <v-icon :icon="link.icon"></v-icon>
+    </template>
+    <v-list-item-title>{{ link.title }}</v-list-item-title>
+</v-list-item>
+
   </v-list>
+  
   </v-navigation-drawer>
   <v-app-bar app dark color="primary">
   <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
   <v-spacer></v-spacer>
   <v-toolbar-items class="hidden-sm-and-down">
   <v-btn
-  text
   v-for="link in links"
   :key="link.title"
+  :to="link.url"
   >
   <v-icon
-  start
-  :icon="link.icon"
+    start
+    :icon="link.icon"
   ></v-icon>
   {{ link.title }}
+
   </v-btn>
   </v-toolbar-items>
   </v-app-bar>
---
+++ frontend/src/views/Auth/RegistrationView.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+    <v-row justify="center">
+    <h1>Registration</h1>
+    </v-row>
+    </v-container>
+    </template>
+    <script>
+    export default {
+    data () {
+    return {
+    }
+    }
+    }
+    </script>
+
+
+<style>
+h1 {
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    color: black
+    
+}
+</style>
+    
