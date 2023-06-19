e5b365297a2d1e2c79876b8b911c525eebe0a319
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-06-19 04:47:33 +0300

    6.3

--- frontend/src/views/User/OrdersView.vue
+++ frontend/src/views/User/OrdersView.vue
@@ -1,16 +1,54 @@
 <template>
     <v-container>
-      <v-row justify="center">
-        <h1>OrdersView</h1>
-      </v-row>
+        <v-row>
+            <v-col cols="6" offset="3">
+                <h1 class="text--secondary mb-3 mt-3">Orders</h1>
+                <v-list subheader two-line flat>
+                    <v-list-item v-for="order in orders"
+                    :key="order.id">
+                        <template>
+                            <v-list-item-action>
+                                <v-checkbox
+                                :input-value="order.done" 
+                                color="primary"
+                                @click="markDone(order)" 
+                                ></v-checkbox>
+                            </v-list-item-action>
+                        </template>
+                        <v-list-item-title>{{ order.name }}</v-list-item-title>
+                        <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
+                        <template v-slot:append>
+                                <v-list-item-action>
+    <v-btn class="primary" :to="'/ad/' + order.adId">Open</v-btn>
+        </v-list-item-action>
+                        </template>
+                    </v-list-item>
+                </v-list>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-      }
+export default {
+    data() {
+        return {
+            orders: [
+                {
+                    id: "123",
+                    name: "Kostya",
+                    phone: "+7(978)000-00-05",
+                    adId: "1",
+                    done: true
+                }
+            ]
+        }
+    },
+    methods: {
+        markDone(order) {
+            order.done = !order.done
+            console.log(order.done)
+        }
     }
-  }
+}
 </script>
